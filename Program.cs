fc57bc0eefe11c95fdf42a8f1c36bdd4710a5af8
KerinAO <90246117+KerinAO@users.noreply.github.com>
2022-09-26 10:59:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanjeZadatak7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool prim = false;
+            int broj = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                for (int j = 2; j <= 100; j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        prim = false;
+                        break;
+                    }
+                }
+                if (prim)
+                {
+                    broj++;
+                }
+                prim = true;
+            }
+
+            Console.WriteLine(broj);
+            Console.ReadKey();
+        }
+    }
+}
